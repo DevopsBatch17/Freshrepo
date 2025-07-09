0e84feb4095ccd38e2dd10cfa0464674906f35dd
rahulpateriya <rahulpateriya287@gmai.com>
2025-07-09 20:43:48 +0530

    add fist file

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "resource_rp" {
+  for_each = tomap({
+    "rg1" = "East US"
+    "rg2" = "West US"
+    "rg3" = "Central US"
+  })
+  name     = each.key
+  location = each.value
+}
