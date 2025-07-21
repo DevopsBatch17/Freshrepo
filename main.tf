358b5596bd12b3d6245c7fb2e3909899fe0caba4
rahulpateriya <rahulpateriya287@gmail.com>
2025-07-21 18:09:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-resource "azurerm_resource_group" "resource_rp" {
-  for_each = tomap({
-    "rg1" = "East US"
-    "rg2" = "West US"
-    "rg3" = "Central US"
-  })
-  name     = each.key
-  location = each.value
-}
 resource "azurerm_resource_group" "rg1" {
 name    = "rgdelhi"
 location = "East US"  
